991c751a08b5ec7181f07464fd685325f07aeddc
Victor Cotrim <cotrim149@gmail.com>
2018-05-08 18:16:32 -0300

    Adding oppacity buttons to modal and styling them

--- components/NewRepoModal.js
+++ components/NewRepoModal.js
@@ -16,7 +16,26 @@ export default class NewRepoModal extends Component {
 			<Modal animationType = 'fade' transparent = {true} visible = {true}>
 				<View style={styles.modalContainer}>
 					<View style={styles.boxContainer}>
+						<Text style={styles.boxTitle}>Adicionar repositório</Text>
+						<TextInput autoFocus autoCapitalization='none' style={styles.boxInput} underlineColorAndroid = 'rgba(0,0,0,0)' placeholder = 'organização/repositório'></TextInput>
 
+						<View style={styles.buttonsContainer}>
+
+							<TouchableOpacity
+								style={[styles.button, styles.cancelButton]}
+								onPress= { () => {} }
+							>
+								<Text style={styles.buttonText}>Cancelar</Text>
+							</TouchableOpacity>
+
+							<TouchableOpacity
+								style={[styles.button, styles.submitButton]}
+								onPress= { () => {} }
+							>
+								<Text style={styles.buttonText}>Adicionar</Text>
+							</TouchableOpacity>
+
+						</View>
 					</View>
 				</View>
 			</Modal>
@@ -37,5 +56,43 @@ const styles = StyleSheet.create ({
 			backgroundColor: '#FFF',
 			alignItems: 'center',
 			width: 280,
+		},
+		boxTitle: {
+			fontWeight: 'bold',
+			fontSize: 16,
+		},
+		boxInput: {
+			alignSelf: 'stretch',
+			marginTop: 10,
+			paddingVertical: 0,
+			paddingHorizontal: 20,
+			borderWidth: 1,
+			borderColor: '#DDD',
+			height: 40,
+			borderRadius: 3,
+		},
+		buttonsContainer: {
+			marginTop: 10,
+			height: 40,
+			flexDirection: 'row'
+		},
+		button: {
+			flex: 1,
+			alignItems: 'center',
+			justifyContent: 'center',
+			borderRadius: 3,
+		},
+		cancelButton: {
+			backgroundColor: '#E25F5F',
+			marginRight: 5,
+		},
+		submitButton: {
+			backgroundColor: '#70BD85',
+			marginLeft: 5,
+		},
+		buttonText: {
+			fontWeight: 'bold',
+			color: '#FFF',
+			fontSize: 12
 		}
 });
