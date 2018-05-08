d0e2aef2d522d26af9d4121933f76df83c02ccac
Victor Cotrim <cotrim149@gmail.com>
2018-05-08 17:00:23 -0300

    Adding all styles to componet

--- componets/Repo.js
+++ componets/Repo.js
@@ -28,8 +28,28 @@ const styles = StyleSheet.create({
 	repo: {
 		padding: 20,
 		backgroundColor: '#FFF',
-		height: 120,
 		marginBottom: 20,
 		borderRadius: 5,
+		flexDirection: 'row',
+		alignItems: 'center',
 	},
+	repoImage: {
+		height: 50,
+		width: 50,
+		borderRadius: 25,
+	},
+
+	repoInfo: {
+		marginLeft: 10,
+	},
+
+	repoTitle: {
+		fontWeight: 'bold',
+		color: '#333'
+	},
+
+	repoAuthor: {
+		fontSize: 13,
+		color: '#999'
+	}
 });
